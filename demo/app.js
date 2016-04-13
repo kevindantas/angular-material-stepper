adfaee411ce7bf236f58ea02f995932ee49e83c4
kevindantas <kevin.ds.29@gmail.com>
2016-04-13 10:45:31 -0300

    Mock form on demo

--- demo/app.js
+++ demo/app.js
@@ -3,30 +3,8 @@
  */
 
 angular.module('myApp', ['kds.stepper', 'textAngular']);
-angular.module('myApp').config(function($mdThemingProvider) {
-  $mdThemingProvider.definePalette('myNewPaletteName', {
-    '50': 'ffebee',
-    '100': 'ffcdd2',
-    '200': 'ef9a9a',
-    '300': 'e57373',
-    '400': 'ef5350',
-    '500': 'f44336',
-    '600': 'e53935',
-    '700': 'd32f2f',
-    '800': 'c62828',
-    '900': 'b71c1c',
-    'A100': 'ff8a80',
-    'A200': 'ff5252',
-    'A400': 'ff1744',
-    'A700': 'd50000',
-    'contrastDefaultColor': 'light',    // whether, by default, text (contrast)
-                                        // on this palette should be dark or light
-    'contrastDarkColors': ['50', '100', //hues which contrast should be 'dark' by default
-      '200', '300', '400', 'A100'],
-    'contrastLightColors': undefined    // could also specify this if default was 'dark'
-  });
-  $mdThemingProvider.theme('mytheme')
-      .primaryPalette('myNewPaletteName')
+angular.module('myApp').config(function () {
+
 });
 
 angular.module('myApp').controller('myController', function($scope, $timeout){
@@ -37,10 +15,7 @@ angular.module('myApp').controller('myController', function($scope, $timeout){
     step3: false
   };
 
-
-  $scope.user = {
-    nome: 'dawdaw'
-  };
+  $scope.user = {};
 
   $scope.messages = {
     begin: 'Carregando com interpolation ...',
@@ -50,14 +25,21 @@ angular.module('myApp').controller('myController', function($scope, $timeout){
 
   $scope.isLoading = false;
 
-  $scope.beginStep = function() {
+
+  $scope.create = function () {
+    $scope.steps.step2 = true;
+  };
+
+
+  $scope.beginStep = function () {
     $scope.isLoading = true;
     $timeout(function() {
       $scope.steps.begin = true;
       $scope.isLoading = false;
       $scope.steps.begin = true;
     }, 2000);
-  }
+  };
+
 
   /**
    * @description
@@ -73,8 +55,6 @@ angular.module('myApp').controller('myController', function($scope, $timeout){
       $scope.steps.fillForm.loading = false;
       $scope.steps.fillForm.done = true;
     }, 5000);
-
   }
-
 
 });
